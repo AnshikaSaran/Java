64443be2e1417616683726f61f5f220392b3bbf4
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-04 20:49:03 +0530

    Create majorityElement.java

---
+++ Arrays/majorityElement.java
@@ -0,0 +1,10 @@
+public static int majorityElement(int[] arr) {
+    int count = 0, candidate = 0;
+    for (int num : arr) {
+        if (count == 0) {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+    return candidate;
+}
