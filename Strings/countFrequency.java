e68a4560cd5b5dfd73b0d8b7db8896b14bda7c36
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-04 21:24:53 +0530

    Create countFrequency.java

---
+++ Strings/countFrequency.java
@@ -0,0 +1,8 @@
+public static Map<Character, Integer> countFrequency(String str) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+        return frequencyMap;
+    }
+}
