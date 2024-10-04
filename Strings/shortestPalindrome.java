23deb6b3972b0b79eaa7646f9e61ece3c3ea0b0b
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-04 21:36:26 +0530

    Create shortestPalindrome.java

---
+++ Strings/shortestPalindrome.java
@@ -0,0 +1,10 @@
+public static String shortestPalindrome(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        for (int i = 0; i <= s.length(); i++) {
+            if (s.startsWith(rev.substring(i))) {
+                return rev.substring(0, i) + s;
+            }
+        }
+        return "";
+    }
+
