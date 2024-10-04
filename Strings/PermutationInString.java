df7d09f76f10bc4a4528d599e99c31429a892c0f
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-04 23:00:44 +0530

    Create PermutationInString.java

---
+++ Strings/PermutationInString.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+            if (matches(s1Count, s2Count)) return true;
+            s2Count[s2.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i - s1.length()) - 'a']--;
+        }
+
+        return matches(s1Count, s2Count);
+    }
+
+    public static boolean matches(int[] s1Count, int[] s2Count) {
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] != s2Count[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        System.out.println(checkInclusion(s1, s2)); // true
+    }
+}
+
+
