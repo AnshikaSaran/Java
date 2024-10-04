a99b72d525aa2d1aca9d7fafe05a850628a717eb
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-04 23:15:07 +0530

    Create MinStack.java

---
+++ Stack/MinStack.java
@@ -0,0 +1,33 @@
+public class MinStack {
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        mainStack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (mainStack.isEmpty()) return;
+
+        int top = mainStack.pop();
+        if (top == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
