ee44a38f3852d036917617ff15d203f26ceefe97
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-04 21:25:48 +0530

    Create convertFirstLetterToUppercase.java

---
+++ Strings/convertFirstLetterToUppercase.java
@@ -0,0 +1,7 @@
+public static String convertFirstLetterToUppercase(String sentence) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase();
+        }
+        return String.join(" ", words);
+    }
