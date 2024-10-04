fe3f3e2ca534532202209f08cfcbfe60251fdec2
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-04 20:48:21 +0530

    Create moveZerosToEnd.java

---
+++ Arrays/moveZerosToEnd.java
@@ -0,0 +1,11 @@
+public static void moveZerosToEnd(int[] arr) {
+    int nonZeroIndex = 0;
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] != 0) {
+            arr[nonZeroIndex++] = arr[i];
+        }
+    }
+    while (nonZeroIndex < arr.length) {
+        arr[nonZeroIndex++] = 0;
+    }
+}
