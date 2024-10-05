d9f912ceaecdb6a35df11a9a53c529a379274bc4
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-05 13:06:22 +0530

    Create inheritence.java

---
+++ oopsConcept/inheritence.java
@@ -0,0 +1,20 @@
+//Inheritance allows a class to inherit properties and methods from another class.
+  class Animal {
+    public void speak() {
+        System.out.println("Animal speaks");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InheritanceExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.speak(); // Outputs: Dog barks
+    }
+}
