51e768a4ea04953b9e3b4e03d94f43676787b7aa
AnshikaSaran <anshika.saran2003@gmail.com>
2024-10-05 13:12:53 +0530

    Update Abstraction.java

--- oopsConcept/Abstraction.java
+++ oopsConcept/Abstraction.java
@@ -54,3 +54,50 @@ public class AbstractionExample {
         System.out.println("Area: " + rectangle.area());
     }
 }
+
+
+
+//Problem: Create an abstract class Shape and two derived classes Circle and Rectangle. Implement a method to calculate the area for each shape.
+abstract class Shape {
+    abstract double area();
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    double area() {
+        return width * height;
+    }
+}
+
+// Example usage
+public class AbstractionExample {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+        System.out.println("Circle Area: " + circle.area());
+        System.out.println("Rectangle Area: " + rectangle.area());
+    }
+}
+
+
